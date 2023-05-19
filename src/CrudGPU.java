4be86cf87c1b01f8f6a02fbbd6bb100164a8693e
Andre <andre.betetto@unesp.br>
2023-05-19 10:47:06 -0300

    feat:

---
+++ src/CrudGPU.java
@@ -0,0 +1,225 @@
+import java.util.Scanner; //teclado
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CrudGPU {
+    public static String xopcao;
+	public static Scanner teclado;
+	public static ArrayList listaliga;
+
+	//Campos cadastro ////////////
+	public static int xid;
+	public static String xnome;
+	public static String xfab;
+	public static String xmem;
+	public static String xpreco;
+
+	public static double preco;
+	public static int mem;
+	///////////////////////////////////
+
+	public static String Scanf (String texto)
+	{
+		System.out.print(texto);
+		xopcao = teclado.nextLine();
+		return xopcao;
+	}
+	
+	public static void MSG (String texto)
+	{
+		System.out.print(texto);
+		teclado.nextLine();
+	}
+	
+	public static void cls()
+	{
+		for(int linha = 0; linha < 20; linha++);
+		System.out.println(""); 
+	}
+	
+	public static void main (String tx[])
+	{
+		teclado = new Scanner(System.in);
+		listaliga = new ArrayList<GPU>();
+		do
+		{
+			cls();	
+			System.out.print("\n<<Crud - GPU >>");
+			System.out.print("\n<1> - Incluir GPU......");
+			System.out.print("\n<2> - Excluir GPU......");
+			System.out.print("\n<3> - Listar GPU......");
+			System.out.print("\n<4> - Alterar GPU......");
+			System.out.print("\n<5> - Ler arquivo.txt......");
+			System.out.print("\n<6> - Gravar arquivo.txt......");
+			System.out.print("\n<7> - Fim programa......");
+			xopcao=Scanf("\n Digite a sua opcao (1-7): ");
+			
+			try{
+				int op = Integer.parseInt(xopcao);
+				if(op < 1 || op > 7) MSG("Opção inválida!! <enter>");
+				if(op==1) Incluir();
+				if(op==2) Excluir();
+				if(op==3) Listar();
+				if(op==4) Alterar();
+				if(op==5) Ler();
+				if(op==6) Gravar();
+				if(op == 7) System.exit(0);
+				
+			}catch(Exception e)
+			{
+				MSG("Valor inválido!! <enter>");
+			}
+			
+		}while(true);
+	}//main	
+	public static int CriaID()
+	{
+		if(listaliga.isEmpty())return 1;
+		int tam=listaliga.size();
+		int ult=tam-1;
+		GPU aux = (GPU)listaliga.get(ult);
+		return aux.getId()+1;
+		
+	}
+
+	public static void Incluir()
+	{
+		cls();
+		System.out.println(">>Incluir GPU");
+		System.out.println(">>Digite os dados da GPU>>"+CriaID());
+		xnome = Scanf("Nome da GPU: ");
+		xfab = Scanf("Fabricante da GPU: ");
+		xmem = Scanf("Memoria da GPU: ");
+		xpreco = Scanf("Preço da GPU: ");
+
+		System.out.println("Dados digitados:");
+		System.out.println("Nome: "+xnome);
+		System.out.println("Fabricante: "+xfab);
+		System.out.println("Memoria: "+xmem);
+		System.out.println("Preço: "+xpreco);
+		xopcao = Scanf("Confirmar dados digitados(s/n)?");
+		// if(xopcao.equals("s") || xopcao.equals("S"))
+		if(xopcao.toUpperCase().equals("S"))
+		{
+			GPU aux = new GPU();
+			aux.setId(CriaID());
+			aux.setNome(xnome);
+			aux.setFab(xfab);
+			
+			try {
+				preco = Double.parseDouble(xpreco);
+			}catch(Exception errpreco)
+			{
+				MSG("Valor inválido!! <enter>"); preco = 1.00;
+			}
+			aux.setPreco(preco);
+			try {
+				mem = Integer.parseInt(xmem);
+			}catch(Exception errmem)
+			{
+                MSG("Valor inválido!! <enter>"); mem = 1;
+            }
+			aux.setMem(mem);
+			listaliga.add(aux);
+			MSG("Cadastrado com sucesso!");
+		}
+
+	}
+	
+	public static void Excluir()
+	{
+		cls();
+		System.out.println(">>Excluir GPU");
+		System.out.println(">>Digite o ID da GPU>>");
+		xid = Integer.parseInt(Scanf("ID da GPU: "));
+		int tam = listaliga.size();
+		for(int i=0; i<tam; i++)
+		{
+			GPU aux = (GPU)listaliga.get(i);
+			if(aux.getId() == xid)
+			{
+				System.out.println("Dados da GPU:");
+				System.out.println("ID: "+aux.getId());
+				System.out.println("Nome: "+aux.getNome());
+				System.out.println("Fabricante: "+aux.getFab());
+				System.out.println("Memoria: "+aux.getMem());
+				System.out.println("Preço: "+aux.getPreco());
+				xopcao = Scanf("Confirmar exclusão(s/n)?");
+				if(xopcao.equals("s") || xopcao.equals("S"))
+				{
+					listaliga.remove(i);
+					MSG("Excluido com sucesso!");
+				}
+				else
+				{
+					MSG("Exclusão cancelada!");
+				}
+				return;
+			}
+		}
+		MSG("ID não encontrado!");
+	}
+	
+	public static void Alterar()
+	{
+		cls();
+		System.out.println(">>Alterar GPU");
+		System.out.println(">>Digite o ID da GPU>>");
+		xid = Integer.parseInt(Scanf("ID da GPU: "));
+		int tam = listaliga.size();
+	}
+	
+	public static void Listar()
+	{
+		cls();
+		if(listaliga.isEmpty())
+		{
+			MSG("Lista vazia!! <enter>");
+			return;
+		}
+		for(int i=0; i<listaliga.size(); i++)
+		{
+			GPU aux = new GPU();
+			aux = (GPU)listaliga.get(i);
+			System.out.println("ID: "+aux.getId());
+			aux.toStr();
+			System.out.println("--------------------------");
+		}
+	}
+
+	public static void ListaremLinha(){
+		cls();
+		if(listaliga.isEmpty())
+		{
+			MSG("Lista vazia!! <enter>");
+			return;
+		}
+		System.out.println("ID\tNome\tFabricante\tMemoria\tPreço");
+		for(int i=0; i<listaliga.size(); i++)
+		{
+			GPU aux = new GPU();
+			aux = (GPU)listaliga.get(i);
+			System.out.println("ID: "+aux.getId());
+			aux.emLinha();
+			System.out.println("--------------------------");
+		}
+	}
+	
+	public static void Ler()
+	{
+		cls();
+        System.out.println(">>Ler GPU");
+        System.out.println(">>Digite o ID da GPU>>");
+        xid = Integer.parseInt(Scanf("ID da GPU: "));
+        int tam = listaliga.size();
+	}
+	
+	public static void Gravar()
+	{
+		cls();
+        System.out.println(">>Gravar GPU");
+        System.out.println(">>Digite o ID da GPU>>");
+        xid = Integer.parseInt(Scanf("ID da GPU: "));
+        int tam = listaliga.size();
+	}
+}
---
+++ src/GPU.java
@@ -0,0 +1,81 @@
+class GPU {
+	private int id_gpu;
+	private String nome_gpu,fab_gpu;
+	private int mem_gpu; 
+	private double preco_gpu;
+	
+	GPU()
+	{
+		setId(0);
+		setNome("");
+		setFab ("");
+		setMem(0);
+		setPreco(0.0);
+	}
+	
+	public void setId(int i)
+	{
+		this.id_gpu = i;
+	}
+	
+	public void setNome(String n)
+	{
+		this.nome_gpu = n;
+	}
+	
+	public void setFab(String f)
+	{
+		this.fab_gpu = f;
+	}
+	
+	public void setMem(int m)
+	{
+		this.mem_gpu = m;
+	}
+	
+	public void setPreco(double p)
+	{
+		this.preco_gpu = p;
+	}
+	
+	public int getId()
+	{
+		return this.id_gpu;
+	}
+	
+	public String getNome()
+	{
+		return this.nome_gpu;
+	}
+	
+	public String getFab()
+	{
+		return this.fab_gpu;
+	}
+	
+	public int getMem()
+	{
+		return this.mem_gpu;
+	}
+	
+	public double getPreco()
+	{
+		return this.preco_gpu;
+	} 
+
+	public void toStr(){
+		System.out.println("Dados da GPU:");
+		System.out.println("ID: "+getId());
+		System.out.println("Nome: "+getNome());
+		System.out.println("Fabricante: "+getFab());
+		System.out.println("Memoria: "+getMem());
+		System.out.println("Preço: "+getPreco());
+	}
+
+	public void emLinha(){
+		System.out.println("ID: "+getId()+"\tNome: "+getNome()+"\tFabricante: "+getFab()+"\tMemoria: "+getMem()+"\tPreço: "+getPreco());
+	}
+	
+}
+	
+
